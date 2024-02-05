66f097d955d12d5be6c3a860eb75592a5ec55ac6
Damien J. Hoffman <dhoffman41@gatech.edu>
2024-02-04 22:31:56 -0500

    Part Two complete. Probably a better way I could have done that.

--- Day One/src/DayOne.java
+++ Day One/src/DayOne.java
@@ -32,29 +32,76 @@ public class DayOne {
 
         System.out.println(line);
         String parsedInt = "";
-        System.out.println(parsedInt);
+        int lineLength = line.length();
 
-        for(int index = 0; index < line.length();index++){
+        for(int index = 0; index < lineLength;index++){
+            if(Character.isDigit(line.charAt(index))){
+                parsedInt += line.charAt(index);
+                break;
+            }
+            if(lineLength - index >= 3){
+                parsedInt += EvaluateThreeCharacters(line.substring(index,index + 3));
+                if (!parsedInt.isEmpty()){break;}
+
+            }else{continue;}
+            if(lineLength - index >= 4){
+                parsedInt += EvaluateFourCharacters(line.substring(index,index + 4));
+                if (!parsedInt.isEmpty()){break;}
+            }else{continue;}
+            if(lineLength - index >= 5){
+                parsedInt += EvaluateFiveCharacters(line.substring(index,index + 5));
+                if (!parsedInt.isEmpty()){break;}
+            }
+        }
+        System.out.println(parsedInt);
+        for(int index = lineLength - 1; index >= 0;index--){
 
             if(Character.isDigit(line.charAt(index))){
                 parsedInt += line.charAt(index);
                 break;
             }
-
-        }
-        System.out.println(parsedInt);
-        for(int index = line.length() - 1; index >= 0;index--){
-
-            if(Character.isDigit(line.charAt(index))){
-                parsedInt += line.charAt(index);
-                break;
+            if(index >= 2){
+                parsedInt += EvaluateThreeCharacters(line.substring(index - 2,index + 1));
+                if (parsedInt.length() != 1){break;}
+            }else{continue;}
+            if(index >= 3){
+                parsedInt += EvaluateFourCharacters(line.substring(index - 3,index + 1));
+                if (parsedInt.length() != 1){break;}
+            }else{continue;}
+            if(index >= 4){
+                parsedInt += EvaluateFiveCharacters(line.substring(index - 4,index + 1));
+                if (parsedInt.length() != 1){break;}
             }
 
         }
         System.out.println(parsedInt);
-
         return Integer.parseInt(parsedInt);
+    }
 
+    public static String EvaluateThreeCharacters(String substring){
+        return switch (substring) {
+            case "one" -> "1";
+            case "two" -> "2";
+            case "six" -> "6";
+            default -> "";
+        };
+    }
+    public static String EvaluateFourCharacters(String substring){
+        return switch (substring) {
+            case "four" -> "4";
+            case "five" -> "5";
+            case "nine" -> "9";
+            case "zero" -> "0";
+            default -> "";
+        };
+    }
+    public static String EvaluateFiveCharacters(String substring){
+        return switch (substring) {
+            case "three" -> "3";
+            case "seven" -> "7";
+            case "eight" -> "8";
+            default -> "";
+        };
     }
 
 }
